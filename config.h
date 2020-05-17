33581bc7b2ca981958630fb8a0568e5c49097963
Wjinlei <1976883731@qq.com>
2020-05-17 12:25:27 +0800

    Modify font to Jetbrains Mono

--- config.h
+++ config.h
@@ -5,8 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "Jetbrains Mono:pixelsize=20:antialias=true:autohint=true";
+
+static char *font2[] = {
+    "JoyPixels:pixelsize=20:antialias=true:autohint=true" 
+};
+
 static int borderpx = 2;
 
 /*
@@ -253,31 +257,31 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ MODKEY,               XK_Home,        zoomreset,      {.f =  0} },
-	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
-	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
+	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} }, /* 复制 */
+	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} }, /* 粘贴 */
+	{ MODKEY,               XK_v,           clippaste,      {.i =  0} }, /* 粘贴 */
 	{ XK_ANY_MOD,		Button2,	selpaste,	{.i =  0} },
 	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_Control_L,   iso14755,       {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} }, /* 向上跳一页 */
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} }, /* 向下跳一页 */
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} }, /* 向上跳一页 */
+	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} }, /* 向下跳一页 */
+	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} }, /* 向上移动一行 */
+	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} }, /* 向下移动一行 */
+	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} }, /* 向上移动一行 */
+	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} }, /* 向下移动一行 */
+	//{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
+	//{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
 	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
-	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
-	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
-	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } }, /* 打开url */
+	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } }, /* 复制url */
+	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } }, /* 复制输出 */
 };
 
 /*
