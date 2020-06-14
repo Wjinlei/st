4d6a33d58f4e09e3f5a59e08e715f497af360f95
Wjinlei <1976883731@qq.com>
2020-06-15 01:01:20 +0800

    Modify borderpx 2 to 4

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static char *font2[] = {
     "JoyPixels:pixelsize=20:antialias=true:autohint=true"
 };
 
-static int borderpx = 2;
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
