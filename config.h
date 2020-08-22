d6e8539104219a9d64571723e1dae0f326cc5774
Wjinlei <1976883731@qq.com>
2020-08-23 04:34:28 +0800

    modify font to SauceCodePro

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=23:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=23:antialias=true:autohint=true";
 
 static char *font2[] = {
     "JoyPixels:pixelsize=23:antialias=true:autohint=true"
