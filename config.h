bfd2d35a57ad47504c92828f5d18dbf8b95acb17
Wjinlei <1976883731@qq.com>
2021-04-18 04:15:21 +0800

    optimized

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=21:antialias=true:autohint=true";
+static char *font = "JetbrainsMono Nerd Font Mono:pixelsize=19:antialias=true:autohint=true";
 
 static char *font2[] = {
-    "JoyPixels:pixelsize=21:antialias=true:autohint=true"
+    "JoyPixels:pixelsize=19:antialias=true:autohint=true"
 };
 
 static int borderpx = 2;
